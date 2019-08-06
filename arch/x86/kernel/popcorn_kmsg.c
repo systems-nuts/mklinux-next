640be148457352ab3689d847ba5b6cee19823b72
antonio barbalace <abarbala@stevens.edu>
2019-08-06 09:43:49 -0400

    include files updated

--- arch/x86/kernel/popcorn_kmsg.c
+++ arch/x86/kernel/popcorn_kmsg.c
@@ -1,4 +1,38 @@
+/*
+ * Antonio Barbalace, Stevens 2019
+ */
 
+#include <linux/init.h>
+
+/*
+#include <linux/mm.h>
+#include <linux/delay.h>
+#include <linux/spinlock.h>*/
+#include <linux/export.h>
+#include <linux/kernel_stat.h>
+
+/*
+#include <linux/mc146818rtc.h>
+#include <linux/cache.h>
+*/
+#include <linux/interrupt.h>
+#include <linux/cpu.h>
+/*#include <linux/gfp.h>
+*/
+
+/*
+#include <asm/mtrr.h>
+#include <asm/tlbflush.h>
+#include <asm/mmu_context.h>
+*/
+#include <asm/proto.h>
+#include <asm/apic.h>
+/*#include <asm/nmi.h>
+#include <asm/mce.h>
+#include <asm/trace/irq_vectors.h>
+#include <asm/kexec.h>
+#include <asm/virtext.h>
+*/
 
 extern popcorn_kmsg_interupt_handler(struct pt_regs *regs, unsigned long long timestamp);
 
