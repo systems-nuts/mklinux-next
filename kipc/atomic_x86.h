4d19e58554b3ac91cab75f85676b0f326349d4c7
antonio barbalace <abarbala@stevens.edu>
2019-09-26 10:06:50 -0400

    added atomic_x86.h (just to ease life)

---
+++ kipc/atomic_x86.h
@@ -0,0 +1,48 @@
+/*
+ * atomic_x86.c
+ *
+ *  Created on: Dec 7, 2015
+ *      Author: root
+ */
+
+#ifndef PCNMSG_ATOMIC_X86_C_
+#define PCNMSG_ATOMIC_X86_C_
+
+
+/*****************************************************************************/
+/* ARCH DEP */
+/*****************************************************************************/
+
+/* From Wikipedia page "Fetch and add", modified to work for u64 */
+static inline unsigned long fetch_and_add(volatile unsigned long * variable,
+					  unsigned long value)
+{
+	asm volatile(
+		     "lock; xaddq %%rax, %2;"
+		     :"=a" (value)                   //Output
+		     : "a" (value), "m" (*variable)  //Input
+		     :"memory" );
+	return value;
+}
+
+static inline int atomic_add_return_sync(int i, atomic_t *v)
+{
+//based on kernel version xadd_sync has been removed
+#if 0    
+	return i + xadd_sync(&v->counter, i);
+#else
+    return i + xadd(&v->counter, i);
+#endif
+}
+
+static inline int atomic_dec_and_test_sync(atomic_t *v)
+{
+	unsigned char c;
+
+	asm volatile("lock; decl %0; sete %1"
+		     : "+m" (v->counter), "=qm" (c)
+		     : : "memory");
+	return c != 0;
+}
+
+#endif /* PCNMSG_ATOMIC_X86_C_ */
