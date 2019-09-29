176371e7165c480032463fc5767b438246f526f6
Antonio Barbalace <abarbala@stevens.edu>
2019-09-29 19:40:03 -0400

    fixes to the script

--- tools/popcorn/locking/test.sh
+++ tools/popcorn/locking/test.sh
@@ -8,8 +8,8 @@ command -v taskset >/dev/null 2>&1 || { echo >&2 "taskset is required but it's n
 command -v python >/dev/null 2>&1 || { echo >&2 "python is required but it's not installed. Exiting."; exit 1; }
 command -v ../lstopo.py >/dev/null 2>&1 || { echo >&2 "lstopo.py is required but it's missing. Exiting."; exit 1; }
 
-for CPUS in `seq 1 16` ; do
-  MASK=`python -c "print (2 ** $CPUS) -1"`
+for CPUS in `seq 1 144` ; do
+  MASK=`python -c "print (hex(2 ** $CPUS -1))" | sed 's/L//g'`
   echo "running $CPUS with $MASK"
-  taskset $MASK ./test > results$CPUS.dat
+#  taskset $MASK ./test > results$CPUS.dat
 done
