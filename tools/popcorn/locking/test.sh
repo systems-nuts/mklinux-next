8eba0be9a76ed193edb0757cd943066848a9cf3f
Antonio Barbalace <abarbala@stevens.edu>
2019-10-11 09:22:40 -0400

    updated locking test script

--- tools/popcorn/locking/test.sh
+++ tools/popcorn/locking/test.sh
@@ -8,7 +8,32 @@ command -v taskset >/dev/null 2>&1 || { echo >&2 "taskset is required but it's n
 command -v python >/dev/null 2>&1 || { echo >&2 "python is required but it's not installed. Exiting."; exit 1; }
 command -v ../lstopo.py >/dev/null 2>&1 || { echo >&2 "lstopo.py is required but it's missing. Exiting."; exit 1; }
 
-for CPUS in `seq 1 144` ; do
+#configuration
+PREFIX="results"
+EXTENSION=".log"
+
+# get the CPUs list
+TEMP_FILE=`tempfile`
+#echo $TEMP_FILE
+lstopo -p > $TEMP_FILE
+LSTOPO=`../lstopo.py $TEMP_FILE | awk '{print $1}'`
+rm $TEMP_FILE
+
+#new implementation using lstopo -p
+CPUS=""
+for CPU in $LSTOPO ; do
+  [[ ! -z "$CPUS" ]] && CPUS="$CPUS,$CPU" || CPUS="$CPU"
+  NUMCPUS="${CPUS//[^,]}"
+  NUMCPUS=${#NUMCPUS}
+  echo "running with $NUMCPUS CPUs added CPU $CPU"
+  taskset -c $CPUS ./test > $PREFIX$NUMCPUS$EXTENSION
+done
+
+exit
+
+NUMCPUS=`cat /proc/cpuinfo | grep "processor" | wc -l`
+#this is the previous implementation
+for CPUS in `seq 1 $NUMCPUS` ; do
   MASK=`python -c "print (hex(2 ** $CPUS -1))" | sed 's/L//g'`
   TEST_MASK=`python -c "value=((2 ** $CPUS -1))
 count=0
@@ -17,5 +42,5 @@ while (value):
   value >>=1
 print(count)"`
   echo "running $CPUS with $MASK and $TEST_MASK"
-  taskset $MASK ./test > results$CPUS.dat
+  taskset $MASK ./test > $PREFIX$CPUS$EXTENSION
 done
